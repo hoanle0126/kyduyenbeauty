dda20c9db317e8e300946618163e48cbaca4ad7d
hoanle0126 <hoanle0126@gmail.com>
2025-01-25 16:53:05 +0700

    new

--- frontend/src/config/axiosClient.jsx
+++ frontend/src/config/axiosClient.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: `http://localhost:3000/api`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 axiosClient.interceptors.request.use((config) => {
