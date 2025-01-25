8d00573b28aba366ecc25e99ebe68b1061a197d4
hoanle0126 <hoanle0126@gmail.com>
2025-01-25 17:14:23 +0700

    asd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
@@ -8,7 +8,13 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "assets": path.resolve(__dirname, "./src/assets"),
+      assets: path.resolve(__dirname, "./src/assets"),
     },
   },
-})
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "dist",
+  },
+});
