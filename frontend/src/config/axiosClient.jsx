bd3fa2005f74b43963dcc7f6981a953a3d109991
hoanle0126 <hoanle0126@gmail.com>
2025-01-26 15:42:25 +0700

    frontend

--- frontend/src/config/axiosClient.jsx
+++ frontend/src/config/axiosClient.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: `http://localhost:8000/api`,
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
 });
 
 axiosClient.interceptors.request.use((config) => {
